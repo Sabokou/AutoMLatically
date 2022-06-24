6bf1d4f0d24176324b1315f3efb5e907eb45912c
Dominic Viola <60390517+PatchFramework@users.noreply.github.com>
2022-06-24 09:58:40 +0200

    Add: train_test_split and df as output

--- middleware/ML-Framework/Preprocessing.py
+++ middleware/ML-Framework/Preprocessing.py
@@ -7,23 +7,47 @@ from sklearn.compose import make_column_transformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import train_test_split
+
+
 
 
 class Preprocessing:
-    def __init__(self):
-        self.df = pd.read_csv("../public/datafile.csv")
+    def __init__(self, data_path=None):
+        if data_path is None:
+            data_path = "../public/datafile.csv"
+        self.df = pd.read_csv(data_path)
+
+    def train_test_splitter(self, df, y_name, test_size=0.2):
+        """
+        Takes in the dataframe that should be transformed, the column name that is considered the gold label in the training process.
+        The size for the train test split in percent can optionally be set to other values besides 80/20 train/test split.
+        """
+        assert y_name in df.columns, f"The label column '{y_name}' is not in the dataframe columns: {df.columns}"
+        assert type(test_size) == float, f"Please provide a float value between 0 and 1. You provided: {test_size}"
+
+        labels = df[y_name]
+        df.drop(y_name, axis="columns", inplace=True)
+
+        (X_train, X_test, y_train, y_test) = train_test_split(df, labels, test_size=test_size)
+
+        return (X_train, X_test, y_train, y_test)
 
     def linear_preprocessing(self):
         linear_processor_categorical = OneHotEncoder(handle_unknown="ignore")
         linear_processor_numerical = make_pipeline(
-            RobustScaler(), SimpleImputer(strategy="mean", add_indicator=True)
+            RobustScaler(), 
+            SimpleImputer(strategy="mean", add_indicator=True)
         )
 
         linear_preprocessor = make_column_transformer(
-            (linear_processor_numerical, make_column_selector(dtype_include=np.number)), (linear_processor_categorical, make_column_selector(dtype_include=object))
+            (linear_processor_numerical, make_column_selector(dtype_include=np.number)), 
+            (linear_processor_categorical, make_column_selector(dtype_include=object)), 
+            verbose_feature_names_out=False # dont use a prefix that shows the transformations of the column
         )
 
-        LP_df = linear_preprocessor.fit_transform(self.df)
+        # keep the transformed data as a df. See: https://stackoverflow.com/questions/68874492/preserve-column-order-after-applying-sklearn-compose-columntransformer/70526434#70526434
+        LP_df = pd.DataFrame(linear_preprocessor.fit_transform(self.df), columns=linear_preprocessor.get_feature_names_out())
         return LP_df
 
     def tree_preprocessing(self):
@@ -33,10 +57,12 @@ class Preprocessing:
         tree_processor_numerical = SimpleImputer(strategy="mean", add_indicator=True)
 
         tree_preprocessor = make_column_transformer(
-            (tree_processor_numerical, make_column_selector(dtype_include=np.number)), (tree_processor_categorical, make_column_selector(dtype_include=object))
+            (tree_processor_numerical, make_column_selector(dtype_include=np.number)), 
+            (tree_processor_categorical, make_column_selector(dtype_include=object)),
+            verbose_feature_names_out=False
         )
 
-        TP_df = tree_preprocessor.fit_transform(self.df)
+        TP_df = pd.DataFrame(tree_preprocessor.fit_transform(self.df), columns=tree_preprocessor.get_feature_names_out())
         return(TP_df)
 
 if __name__=="__main__":
