df31f4e15fa64e247c855a0f094c075cf7d81a8e
PatchFramework <dominic.viola@online.de>
2022-07-01 11:00:06 +0200

    Add: Boilerplate flask endpoints

--- Flask/src/run.py
+++ Flask/src/run.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import logging
 import os
+import json
 
 PORT = 8001
 # save the uploaded file into this directory inside the container
@@ -27,6 +28,26 @@ def upload_file():
         f.save(os.path.join(BASE_DIR, f.filename))
     return 'file uploaded successfully'
 
+@app.route("/start", methods=['POST'])
+def start_training():
+    if request.method == 'POST':
+        content = json.loads(request.data)
+        app.logger.info(f"You posted the following /start parameters: {content}")
+        return f"You requested /start with the content: {content}"
 
+@app.route("/performance", methods=['GET'])
+def get_perfromance():
+    if request.method == 'GET':
+        app.logger.info(f"You want to GET the /performance parameters")
+        content = json.dumps({"nlpregressor": "0.1234"})
+        return f"Your dummy performance is: {content}"
+
+@app.route("/model-names", methods=['GET'])
+def get_model_names():
+    if request.method == 'GET':
+        app.logger.info(f"You want to GET the /model-names")
+        content = json.dumps({"modelNames": ["nlpregressor", "testclassifier"]})
+        return f"Available ml-models are: {content}"
+        
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=PORT)
